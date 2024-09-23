931fdbb781eb9fd29dfae187ef0ae74aba7fb867
Sanjida <sanjida.ice.778@gmail.com>
2024-09-23 20:16:28 +0600

    utility function added

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function calculateMoney(id1, id2, id3) {
   getElement(id2).innerText = totalDonationAmount;
   const initailBalance = getElement(id3).innerText;
   const currentBalance = initailBalance - donateAmount;
-  return getElement(id3).innerText = currentBalance;
+  return (getElement(id3).innerText = currentBalance);
 }
 
 function getElement(id) {
@@ -16,19 +16,27 @@ function getElement(id) {
 }
 
 // toggle features
+const donationContent = getElement("donation");
+const historyContent = getElement("history");
+const historyBtn = getElement('history-btn');
+const donationBtn = getElement('donation-btn');
 
 document.getElementById("history-btn").addEventListener("click", function () {
-  const donationBtn = getElement("donation");
-  donationBtn.classList.add("hidden");
-  const historyBtn = getElement("history");
-  historyBtn.classList.remove("hidden");
+  donationContent.classList.add("hidden");
+  historyContent.classList.remove("hidden");
+  historyBtn.classList.add('bg-primary');
+  historyBtn.classList.remove('border-border', 'text-text');
+  donationBtn.classList.add('border-border', 'border', 'text-text');
+  donationBtn.classList.remove('bg-primary');   
 });
 
 document.getElementById("donation-btn").addEventListener("click", function () {
-  const historyBtn = getElement("history");
-  historyBtn.classList.add("hidden");
-  const donationBtn = getElement("donation");
-  donationBtn.classList.remove("hidden");
+  historyContent.classList.add("hidden");
+  donationContent.classList.remove("hidden");
+  donationBtn.classList.add('bg-primary');
+  donationBtn.classList.remove('border-border', 'text-text');
+  historyBtn.classList.add('border-border', 'text-text');
+  historyBtn.classList.remove('bg-primary');
 });
 
 // donate button function
@@ -36,17 +44,44 @@ document.getElementById("donation-btn").addEventListener("click", function () {
 document
   .getElementById("donate-now-btn1")
   .addEventListener("click", function () {
+
     calculateMoney("donate-amount1", "total-donate-1", "initial-balance");
+    
+    historyRow("donate-amount1");
+    
+    
   });
+
+function historyRow(id) {
+    const donateAmount = getElement(id).value;
+    const div = document.createElement('div');
+    div.className = "border-2 rounded-2xl p-8 mb-6";
+    let loc = getElement(id).previousElementSibling.previousElementSibling.innerText;
+    console.log(loc)
+    let now = new Date();
+    div.innerHTML = `
+    <h2 class = "font-bold text-xl">${donateAmount} Taka is ${loc}</h2>
+    <p class = "font-light">
+    Date : ${now}
+    </p>
+    `;
+    const history = getElement("history");
+    return history.insertBefore(div, history.firstChild);
+}
 
 document
   .getElementById("donate-now-btn2")
   .addEventListener("click", function () {
+
     calculateMoney("donate-amount2", "total-donate-2", "initial-balance");
+
+    historyRow("donate-amount2");
   });
 
 document
   .getElementById("donate-now-btn3")
   .addEventListener("click", function () {
     calculateMoney("donate-amount3", "total-donate-3", "initial-balance");
+   
+    historyRow('donate-amount3');
   });
